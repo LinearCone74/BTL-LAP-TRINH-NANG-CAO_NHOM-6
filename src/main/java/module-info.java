3d7559c42a4ba16d9ee5e3fa01d67691f795ebe5
LinearCone74 <130677881+LinearCone74@users.noreply.github.com>
2026-03-24 11:23:07 +0700

    fix file module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,6 @@
 module com.example.dau_gia_truc_tuyen {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.dau_gia_truc_tuyen to javafx.fxml;
     exports com.example.dau_gia_truc_tuyen;
