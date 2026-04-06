98ff0d7d133c9cd3074aa6f7a9f52de3dc87b343
Hoang Thai Bao <hoangthaibaomc@gmail.com>
2026-04-06 20:39:31 +0700

    test

---
+++ src/main/java/com/example/dau_gia_truc_tuyen/Bidder.java
@@ -0,0 +1,7 @@
+package com.example.dau_gia_truc_tuyen;
+
+public class Bidder extends User {
+    public Bidder(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/com/example/dau_gia_truc_tuyen/Seller.java
@@ -0,0 +1,7 @@
+package com.example.dau_gia_truc_tuyen;
+
+public class Seller extends User{
+    public Seller(String Name){
+        super(Name);
+    }
+}
