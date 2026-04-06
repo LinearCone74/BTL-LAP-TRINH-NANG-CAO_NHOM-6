ba7958828aaa1266dd6ac11bf607c6519e3856b0
LinearCone74 <130677881+LinearCone74@users.noreply.github.com>
2026-04-06 20:36:01 +0700

    add quan ly nguoi dung va quan ly san pham

--- src/main/java/com/example/dau_gia_truc_tuyen/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.dau_gia_truc_tuyen;
-
-public abstract class User {
-    protected String name;
-
-    public User(String name) {
-        this.name = name;
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.dau_gia_truc_tuyen {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.dau_gia_truc_tuyen to javafx.fxml;
-    exports com.example.dau_gia_truc_tuyen;
-}
